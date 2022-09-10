27b3c3d2658e30effcba3751faaf5c8cf935f107
Joe Mendez <13544990+Joseph-Mendez@users.noreply.github.com>
2022-09-09 19:16:46 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,12 @@ $(window).on("load", function() {
 
 
 $(document).ready(function() {
+	
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
 
 	var typed = new Typed(".typed", {
 		strings: ["Product Designer"],
